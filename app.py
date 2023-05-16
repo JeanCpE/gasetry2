6cf52d338fce855ce63855c63500adbe1135da48
JeanCpE <132177343+JeanCpE@users.noreply.github.com>
2023-05-16 20:19:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import numpy as np
 
 import os
 import subprocess
-import detect1
+import detect
 import argparse
 
 from IPython.display import display
@@ -49,23 +49,23 @@ def main():
             #Show image save success
             st.write("File saved!")
 
-        # Change detect1 parameters
-        if 'weights' not in [action.dest for action in detect1.parser._actions]:
-            detect1.parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
+        # Change detect parameters
+        if 'weights' not in [action.dest for action in detect.parser._actions]:
+            detect.parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
             print('weights args added')
-        if 'source' not in [action.dest for action in detect1.parser._actions]:
-            detect1.parser.add_argument('--source', type=str, default='inference/images',
+        if 'source' not in [action.dest for action in detect.parser._actions]:
+            detect.parser.add_argument('--source', type=str, default='inference/images',
                             help='source')  # file/folder, 0 for webcam
             print('source args added')
-        if 'conf_thres' not in [action.dest for action in detect1.parser._actions]:
-            detect1.parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+        if 'conf_thres' not in [action.dest for action in detect.parser._actions]:
+            detect.parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
             print('conf args added')
 
-        detect1.parser.set_defaults(weights='runs/weights/exp-18-last.pt', conf_thres=0.1, source=("uploads/"+file_path))
-        args = detect1.parser.parse_args()
+        detect.parser.set_defaults(weights='runs/weights/exp-18-last.pt', conf_thres=0.1, source=("uploads/"+file_path))
+        args = detect.parser.parse_args()
         print('Arguments passed:',args)
 
-        gaseDF = detect1.main(args)
+        gaseDF = detect.main(args)
 
         # Specify the directory to scan
         directory = 'runs/detect'
