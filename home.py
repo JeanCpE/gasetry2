cd4bbe2679ef9a0080139c1733dba1b9cd6a6d41
JeanCpE <132177343+JeanCpE@users.noreply.github.com>
2023-05-22 15:27:50 +0800

    Update home.py

--- home.py
+++ home.py
@@ -137,7 +137,7 @@ hide_streamlit_style = """
             </style>
             """
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 # st.markdown("<h1 style='text-align: center;'>Gase App</h1>", unsafe_allow_html=True)
 
 
